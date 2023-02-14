6ee33615f961d96ad97a991647fa71e9f8f163b0
IMMORTALSCAR17 <brandon@w3btw.com>
2023-02-13 21:13:40 -0600

    New version
    Able to have a description

--- TaskManagerV2.java
+++ TaskManagerV2.java
@@ -1,57 +1,55 @@
-//TaskManager.java - Recreated by: ImmortalScar17#3644
-
+//TaskManagerV2.java - Recreated by: ImmortalScar17#3644
 //Imports
 import java.util.*;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class TaskManagerV2 {
-    public static void main(String[] args) throws Exception {
-        try (//Scanner
-        Scanner scan = new Scanner(System.in)) {
+    static void endCommand(String taskname){
+        Scanner scan = new Scanner(System.in);
+        char[] animationChars = new char[] { '|', '/', '-', '\\' };
+        if (taskname.toLowerCase().equals("end")){
+            System.out.println("\n");
+            for (int i = 0; i <= 100; i++) {
+                System.out.print("Ending Task Manager... " + i + "% " + animationChars[i % 4] + "\r");
+                try {
+                    Thread.sleep(15);
+                } catch (InterruptedException e) {
+                    scan.close();
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("\nTask Manager has been successfully terminated!\nNo tasklist was created.");
+            scan.close();
+            System.exit(0);
+        }
+    }
+    public static void main(String[] args)throws Exception {
+        try (Scanner scan = new Scanner(System.in)){
             int end = 5;
             int taskNum = 0;
-            char[] animationChars = new char[] { '|', '/', '-', '\\' };
-
-            //Menu Display
             System.out.println(
                 "\nWelcome to Task Manager BETA!\nWhile entering your tasks, type \"end\" to end task creation and move on.");
-                
             //Name input
             System.out.print("Enter Checklist Name;\n=> ");
             String checklistFileName = scan.nextLine();
-
             //Start of Task
             while (end == 5){
                 System.out.print("Enter name of task;\n=> ");
                 String taskname = scan.nextLine();
-
-                //"end" Check
-                if (taskname.toLowerCase().equals("end")){
-                    System.out.println("\n");
-
-                    //Load exit on "end" command
-                    for (int i = 0; i <= 100; i++) {
-                        System.out.print("Ending Task Manager... " + i + "% " + animationChars[i % 4] + "\r");
-                        try {
-                            Thread.sleep(15);
-                        } catch (InterruptedException e) {
-                            scan.close();
-                            e.printStackTrace();
-                        }
-                    }
-                    System.out.println("\nTask Manager has been successfully terminated!\nNo tasklist was created.");
-                    scan.close();
-                    System.exit(0);
+                endCommand(taskname);
+                System.out.print("Enter the description. [If none, type \"none\" to go to next task.]\n=>");
+                String taskDescription = scan.nextLine();
+                endCommand(taskDescription);
+                if (taskDescription.toLowerCase().equals("none")){
+                    taskDescription = "\n";
                 }
-
-                //End of Task
                 System.out.println(taskname + " successfully entered!");
                 taskNum++;
                 if (taskNum == 1){
                     try {
                         FileWriter fileWrite = new FileWriter(checklistFileName + "'s Tasklist.txt");
-                        fileWrite.write(checklistFileName + "'s Tasklist:\n\nTask"+ taskNum +": " + taskname);
+                        fileWrite.write(checklistFileName + "'s Tasklist:\n\nTask"+ taskNum +": " + taskname + "\nDescription: " + taskDescription);
                         fileWrite.close();
                     } catch (IOException e) {
                         System.out.println("There was an error creating your tasklist file.");
@@ -62,7 +60,7 @@ public class TaskManagerV2 {
                 else{
                     try {
                         FileWriter fileWrite = new FileWriter(checklistFileName + "'s Tasklist.txt", true);
-                        fileWrite.write("\nTask"+ taskNum +": " + taskname);
+                        fileWrite.write("\nTask"+ taskNum +": " + taskname + "\nDescription: " + taskDescription);
                         fileWrite.close();
                     } catch (IOException e) {
                         System.out.println("There was an error editing your tasklist file.");
